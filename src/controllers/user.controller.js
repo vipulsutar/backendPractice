75bc2b352097d43ce9f3d0a04285096b0c3b0d30
vipulsutar <sutarvipul675@gmail.com>
2024-02-19 00:04:21 +0530

    login & logout controller with auth middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,25 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { Apiresponse } from "../utils/ApiResponse.js";
 import mongoose from "mongoose";
 
+const generateRefreshAndAccessToken = async(userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccesToken()
+        const refreshToken = user.generateRefreshToken() 
+
+
+        //saving into the database
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave : false})
+
+        //access token generated
+        return {accessToken,refreshToken}
+
+    } catch (error) {
+        throw new Apierror(500,"Something went wrong")
+    }
+}
+
 const registerUser  = asyncHandler(async (req,res) => {
     //get details from frontend
     //validation - not empty
@@ -65,4 +84,85 @@ const registerUser  = asyncHandler(async (req,res) => {
         new Apiresponse(200,createdUser,"Registertion succesfull")
     )
 })
-export default registerUser;
+
+const loginUser = asyncHandler(async (req,res) => {
+    //req body->data
+    //username or email
+    //find user
+    //check the password
+    //access and refresh token 
+    //send cookies (secure)
+    //response 
+
+
+    const {email,username,password} = req.body
+
+    if(!username || !email){
+        throw new Apierror(400,"Username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or:[{username},{email}]
+    })
+    if(!user){
+        throw new Apierror(400,"User not found");
+    }
+    
+    const passCheck = await user.isPasswordcorrect(password)
+    if(!passCheck){
+        throw new Apierror(401,"Password does not match")
+    }
+
+    const{accessToken,refreshToken} = await generateRefreshAndAccessToken(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+    //cookies
+    const options = {
+        httpOnly : true,    //this makes the cookies unmodifiable which will be visible on frontend as well only modifiable at server
+        secure : true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(
+        new Apiresponse(
+            200,
+            {
+                user:loggedInUser,accessToken,refreshToken
+            },
+            "User logged In Successfully"
+        )
+    )
+
+    
+})
+
+const logoutUser = asyncHandler(async (req,res) => {
+    await User.findByIdAndUpdate(
+            req.user._id,{
+                $set: {
+                    refreshToken:undefined
+                }
+            }
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(
+        new Apiresponse(200,{},"User logged Out Successfully")
+    )
+
+})
+export  {
+    registerUser,
+    loginUser,
+    logoutUser
+};
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import { Apierror } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model";
+
+
+export const verifyJWT = asyncHandler(async (req,res,next) => {
+    try {
+        const token = await req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+    
+        if(!token){
+            throw new Apierror(401,"Unauthorized request")
+        }
+    
+        const decodedToken =  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+        if(!user){
+            throw new Apierror(401,"Invalid Access Token")
+        }
+    
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new Apierror(401,error?.message||"Invalid access token")
+    }
+})
